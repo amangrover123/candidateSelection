8b85a49dfb69de4604db868c0b3e3cd3dcd7f757
Aman Grover <amangrover@Amans-MacBook-Pro.local>
2017-08-24 21:53:43 -0700

    Added candidate selection paper

---
+++ mta-offline-candidate-selection/src/run.sh
@@ -0,0 +1,4 @@
+pdflatex candidate-selection-careers.tex
+bibtex candidate-selection-careers.aux
+pdflatex candidate-selection-careers.tex
+pdflatex candidate-selection-careers.tex
---
+++ mta-offline-candidate-selection/updated-format/run.sh
@@ -0,0 +1,4 @@
+pdflatex candidate-selection-new-format.tex
+bibtex candidate-selection-new-format.aux
+pdflatex candidate-selection-new-format.tex
+pdflatex candidate-selection-new-format.tex
